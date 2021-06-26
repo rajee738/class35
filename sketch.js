ca94ee0fa5a6b6f8477dd988901b476a8a818b31
rajee738 <85677273+rajee738@users.noreply.github.com>
2021-06-26 11:33:52 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,50 @@
+var hypnoticBall,database;
+var position;
+
+
+
+var ball;
+function setup(){
+    database = firebase.database();
+    console.log(database);
+    createCanvas(500,500);
+    ball = createSprite(250,250,10,10);
+    ball.shapeColor = "red";
+
+    var ballPosition = database.ref('ball/position');
+    ballPosition.on("value",readPosition,showError);
+}
+
+function draw(){
+    background("white");
+    if(keyDown(LEFT_ARROW)){
+        changePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        changePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        changePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        changePosition(0,+1);
+    }
+    drawSprites();
+}
+function writePosition(x,y){
+    database.ref('ball/position').set({
+        'x':position.x + x,
+        'y': position.y+y
+    })
+}
+
+function readPosition(data){
+    position = data.val();
+    console.log(position.x);
+    ball.x=position.x;
+    ball.y=position.y
+}
+function showError(){
+    console.log("Error in writing to the database");
+}
+
